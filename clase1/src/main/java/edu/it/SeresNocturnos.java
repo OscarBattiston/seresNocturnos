7e145457a8c1a06d29a751fcc704841a4a1cbc27
andy <arteysoft@gmail.com>
2021-12-22 19:23:10 -0300

    commit inicial

---
+++ clase1/src/main/java/edu/it/SeresNocturnos.java
@@ -0,0 +1,10 @@
+package edu.it;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeresNocturnos {
+	public static void main(String[] args) {
+		System.out.println("Inicio App");
+	}
+}
