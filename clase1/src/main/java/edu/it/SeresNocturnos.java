cc4e7a9abe0ad615e543ed67dc4f84d1002b9ad0
andy <arteysoft@gmail.com>
2021-12-22 20:02:03 -0300

    conexion a base

--- clase1/src/main/java/edu/it/SeresNocturnos.java
+++ clase1/src/main/java/edu/it/SeresNocturnos.java
@@ -2,9 +2,14 @@ package edu.it;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import edu.it.components.ConectorJPA;
+
 @SpringBootApplication
 public class SeresNocturnos {
 	public static void main(String[] args) {
 		System.out.println("Inicio App");
+		
+		var connector = new ConectorJPA();
+		connector.getEntityManager();
 	}
 }
---
+++ clase1/src/main/java/edu/it/components/Utiles.java
@@ -0,0 +1,21 @@
+package edu.it.components;
+
+public class Utiles {
+	/*
+	public static Usuario crearUsuario(Faker fkr) {
+		var u = new Usuario();
+		
+		u.id = UUID.randomUUID().toString();
+		u.nombre = fkr.address().firstName();
+		u.apellido = fkr.address().lastName();
+		u.domicilio = String.join(" ", fkr.address().streetName(), 
+				fkr.address().streetAddressNumber());
+		u.pais = fkr.address().country();
+		u.tipoUsuario = (new Random().nextInt() % 2 == 0) ? 
+				TipoUsuario.USUARIO_ESTANDAR :
+				TipoUsuario.USUARIO_ROOT;
+		
+		return u;
+	}
+	*/
+}
